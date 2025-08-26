cb77b148ee2c94e5776b2e92f07327dbfb2b7190
ayush.kanase <kanaseayush4@gmail.com>
2025-08-26 15:30:52 +0530

    message

--- src/App.js
+++ src/App.js
@@ -3,27 +3,36 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const [number,setNumber]=useState()
+  const [number1,setNumber1]=useState()
   const [number2,setNumber2]=useState()
   const [sum,setSum]=useState()
 
-  function handelvalue(event){
-    setNumber(event.target.value);
+
+  function onSubmit(){
+    setSum(number1+number2)
     
   }
-  function onSubmit(){
-    sum=number+number2
-    setNumber2()
+  const handleNum=(event)=>{
+    setNumber1 (parseInt(event.target.value));
+
   }
+    const handleNum2=(event)=>{
+      setNumber2 (parseInt(event.target.value));
+    }
+     
+
   
 
 
 
   return (
     <div>
-      <input name="number" value={number2} onChange={handelvalue}/>
+      <input name="number1" value={number1} onChange={handleNum}/>
+      <input name="number2" value={number2} onChange={handleNum2}/>
+
       
     <button className='btnSubmit' onClick={onSubmit}>submit</button>
+    <div>sum is {sum}</div>
     
     
       </div>
