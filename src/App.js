b0fe25cc02eb8f87ed0b72e8ae8da42b017378ce
ayush.kanase <kanaseayush4@gmail.com>
2025-09-02 15:03:37 +0530

    array of object in table

--- src/App.js
+++ src/App.js
@@ -4,26 +4,32 @@ import { useState } from 'react';
 
 function App() {
 const students = [
-    { id: 1, name: "Ayush", age: 20, marks: 90 },
-    { id: 2, name: "person1", age: 25, marks: 90 },
-    {id:3,name:"person2",age:30,marks:90}
+    { rollNo: 1, name: "Ayush", age: 20, marks: 90,education:"SYBCS" },
+    { rollNo: 2, name: "person1", age: 25, marks: 90 ,education:"TYBCS"},
+    {rollNo:3,name:"person2",age:30,marks:90,education:"TYBCA"},
+    {rollNo:4,name:"person3",age:35,marks:90,education:"TYBCA"}
   ];
 
 
   return (
-    <table border="1">
+    <table border= {1}>
        
           <tr>
+            <th>Roll No.</th>
             <th>Name</th>
             <th>Age</th>
+            <th>Education</th>
             <th>Marks</th>
+            
           </tr>
      
        
           {students.map((singleObject) => (
             <tr key={singleObject}>
+              <td>{singleObject.rollNo}</td>
               <td>{singleObject.name}</td>
               <td>{singleObject.age}</td>
+              <td>{singleObject.education}</td>
               <td>{singleObject.marks}</td>
             </tr>
           ))}
